a248e1d45770a257fdaf8abf9e8a50fa817c3b3c
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-06-10 10:39:09 +0200

    fixed colorpicker

--- .config/waybar/bin/colorpicker.sh
+++ .config/waybar/bin/colorpicker.sh
@@ -2,15 +2,15 @@
 
 # Pick Color Quickly
 
-color=$(grim -g "$(slurp -b 1B1F2800 -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:- | tail -n1 | cut -d' ' -f4)
+color=$(grim -g "$(slurp -b 1B1F2800 -p)" -t ppm - | magick - -format '%[pixel:p{0,0}]' txt:- | tail -n1 | cut -d' ' -f4)
 image=/tmp/${color}.png
 
 main() {
-	if [[ "$color" ]]; then
+	if [ -n "$color" ]; then
 		# copy color code to clipboard
 		echo $color | tr -d "\n" | wl-copy
 		# generate preview
-		convert -size 48x48 xc:"$color" ${image}
+		magick -size 48x48 xc:"$color" ${image}
 		# notify about it
 		notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i ${image} "$color, copied to clipboard."
 	fi
